eb2041b499c264e06519f0de8d5a11f6f0b033e3
PigeonSensei <parkour5813@gmail.com>
2021-12-28 11:13:58 +0900

    -

---
+++ mpu6050/include/mpu6050/mpu6050.h
@@ -0,0 +1,206 @@
+#ifndef MPU6050_H
+#define MPU6050_H
+
+#include <ros/ros.h>
+#include <tf/transform_datatypes.h>
+#include <tf/transform_broadcaster.h>
+
+#include "imu_msgs/RPY.h"
+#include "mpu6050/ResetIMU.h"
+
+#include <sensor_msgs/Imu.h>
+
+#include <wiringPi.h>
+#include <wiringPiI2C.h>
+
+#include <cmath>
+
+#define ADDRESS_AD0_LOW     0x68
+#define ADDRESS_AD0_HIGH    0x69
+
+#define SELF_TEST_X 0x0D
+#define SELF_TEST_Y 0x0E
+#define SELF_TEST_Z 0x0F
+#define SELF_TEST_A 0x10
+#define SMPLRT_DIV       0x19
+#define CONFIG           0x1A
+#define GYRO_CONFIG      0x1B
+#define ACCEL_CONFIG     0x1C
+#define FIFO_EN          0x23
+#define I2C_MST_CTRL     0x24
+#define I2C_SLV0_ADDR    0x25
+#define I2C_SLV0_REG     0x26
+#define I2C_SLV0_CTRL    0x27
+#define I2C_SLV1_ADDR    0x28
+#define I2C_SLV1_REG     0x29
+#define I2C_SLV1_CTRL    0x2A
+#define I2C_SLV2_ADDR    0x2B
+#define I2C_SLV2_REG     0x2C
+#define I2C_SLV2_CTRL    0x2D
+#define I2C_SLV3_ADDR    0x2E
+#define I2C_SLV3_REG     0x2F
+#define I2C_SLV3_CTRL    0x30
+#define I2C_SLV4_ADDR    0x31
+#define I2C_SLV4_REG     0x32
+#define I2C_SLV4_DO      0x33
+#define I2C_SLV4_CTRL    0x34
+#define I2C_SLV4_DI      0x35
+#define I2C_MST_STATUS   0x36
+#define INT_PIN_CFG      0x37
+#define INT_ENABLE       0x38
+#define DMP_INT_STATUS   0x39
+#define INT_STATUS       0x3A
+#define ACCEL_XOUT_H     0x3B
+#define ACCEL_XOUT_L     0x3C
+#define ACCEL_YOUT_H     0x3D
+#define ACCEL_YOUT_L     0x3E
+#define ACCEL_ZOUT_H     0x3F
+#define ACCEL_ZOUT_L     0x40
+#define TEMP_OUT_H       0x41
+#define TEMP_OUT_L       0x42
+#define GYRO_XOUT_H      0x43
+#define GYRO_XOUT_L      0x44
+#define GYRO_YOUT_H      0x45
+#define GYRO_YOUT_L      0x46
+#define GYRO_ZOUT_H      0x47
+#define GYRO_ZOUT_L      0x48
+#define EXT_SENS_DATA_00 0x49
+#define EXT_SENS_DATA_01 0x4A
+#define EXT_SENS_DATA_02 0x4B
+#define EXT_SENS_DATA_03 0x4C
+#define EXT_SENS_DATA_04 0x4D
+#define EXT_SENS_DATA_05 0x4E
+#define EXT_SENS_DATA_06 0x4F
+#define EXT_SENS_DATA_07 0x50
+#define EXT_SENS_DATA_08 0x51
+#define EXT_SENS_DATA_09 0x52
+#define EXT_SENS_DATA_10 0x53
+#define EXT_SENS_DATA_11 0x54
+#define EXT_SENS_DATA_12 0x55
+#define EXT_SENS_DATA_13 0x56
+#define EXT_SENS_DATA_14 0x57
+#define EXT_SENS_DATA_15 0x58
+#define EXT_SENS_DATA_16 0x59
+#define EXT_SENS_DATA_17 0x5A
+#define EXT_SENS_DATA_18 0x5B
+#define EXT_SENS_DATA_19 0x5C
+#define EXT_SENS_DATA_20 0x5D
+#define EXT_SENS_DATA_21 0x5E
+#define EXT_SENS_DATA_22 0x5F
+#define EXT_SENS_DATA_23 0x60
+#define I2C_SLV0_DO      0x63
+#define I2C_SLV1_DO      0x64
+#define I2C_SLV2_DO      0x65
+#define I2C_SLV3_DO      0x66
+#define I2C_MST_DELAY_CTRL   0x67
+#define SIGNAL_PATH_RESET    0x68
+#define USER_CTRL        0x6A
+#define PWR_MGMT_1       0x6B
+#define PWR_MGMT_2       0x6C
+#define FIFO_COUNTH      0x72
+#define FIFO_COUNTL      0x73
+#define FIFO_R_W         0x74
+#define WHO_AM_I         0x75
+
+class Mpu6050
+{
+public:
+    Mpu6050(ros::NodeHandle &n)
+        : publisher_imu_(n.advertise<sensor_msgs::Imu>("imu",1000)),
+          publisher_rpy_(n.advertise<imu_msgs::RPY>("imu/rpy",1000)),
+          service_server_reset_imu_(n.advertiseService("reset_imu", &Mpu6050::ResetIMUServiceCallback, this))
+       {
+          GetROSParam();
+          SetUp();
+          InitSensor();
+          Reset();
+       }
+       ~Mpu6050()
+       {
+
+       }
+
+    bool ResetIMUServiceCallback(mpu6050::ResetIMU::Request &req,
+                                     mpu6050::ResetIMU::Response &res);
+
+    double DegreeToRadian(double value);
+
+    double RadianToDegree(double value);
+
+    void CalculationDeltaTime();
+
+    void InitVariable();
+
+    void Reset();
+
+    void InitCalibration(int value);
+
+    void Calibration();
+
+    void GetROSParam();
+
+    void SetUp();
+
+    void InitSensor();
+
+    short ReadRawData(int addr);
+
+    void GetImuData();
+
+    void CalculationQuaternion(double gx, double gy, double gz, double ax, double ay, double az);
+
+    void UpdateImuData();
+
+    void UpdateTF();
+
+    void UpdateRPY();
+
+    void Update();
+
+    void Publisher();
+
+    void Spin();
+
+private:
+
+    int fd_ = ADDRESS_AD0_LOW;
+    bool ad0_ = false;
+    bool use_tf_ = true;
+    bool use_calibration_ = true;
+    int calibration_times_ = 2000;
+    bool use_fileter_ = true;
+    double beta_ = 0.0001;
+    int acceleration_scale_ = 0;
+    int gyro_scale_ = 0;
+    std::string frame_id_;
+    std::string parents_frame_id_;
+
+    double dt_ = 0.0;
+
+
+    double acceleration_[3];
+    double gyro_[3];
+
+    double average_acceleration_[3];
+    double average_gyro_[3];
+
+    ros::Time time_now_;
+    ros::Time time_last_;
+    tf::Quaternion orientation_;
+    tf::TransformBroadcaster broadcaster_;
+
+    ros::Publisher publisher_imu_;
+    ros::Publisher publisher_rpy_;
+
+    ros::ServiceServer service_server_reset_imu_;
+    bool reset_swith_ = false;
+
+    sensor_msgs::Imu imu_;
+    imu_msgs::RPY rpy_;
+
+    double q0_ = 1.0, q1_ = 0.0, q2_ = 0.0, q3_ = 0.0;	// quaternion [w x y z]
+
+};
+
+	
+#endif // MPU6050_H
---
+++ mpu6050/src/mpu6050.cpp
@@ -0,0 +1,349 @@
+#include "mpu6050/mpu6050.h"
+
+bool Mpu6050::ResetIMUServiceCallback(mpu6050::ResetIMU::Request &req, mpu6050::ResetIMU::Response &res)
+{
+  if(req.command == true)
+  {
+      reset_swith_ = true;
+      ROS_INFO("Receive Service Call Reset IMU : True");
+      res.message = "Receive success";
+      res.result = "true";
+  }
+  return true;
+}
+
+double Mpu6050::DegreeToRadian(double value)
+{
+  return value * (M_PI/180.0);
+}
+
+double Mpu6050::RadianToDegree(double value)
+{
+  return value * (180.0/M_PI);
+}
+
+void Mpu6050::CalculationDeltaTime()
+{
+  double now = time_now_.sec + (time_now_.nsec) * 0.000000001;
+  double last = time_last_.sec + (time_last_.nsec) * 0.000000001;
+  dt_ = now - last;
+}
+
+void Mpu6050::InitVariable()
+{
+  ROS_INFO("InitVariable MPU6050");
+  for (int i=0;i<=3;i++)
+  {
+    acceleration_[i] = 0.0;
+    gyro_[i] = 0.0;
+
+    average_acceleration_[i] = 0.0;
+    average_gyro_[i] = 0.0;
+  }
+}
+
+void Mpu6050::Reset()
+{
+
+  ROS_INFO("Reset MPU6050");
+  InitVariable();
+  q0_ = 1.0; q1_ = 0.0; q2_ = 0.0; q3_ = 0.0;
+  if(use_calibration_ == true) InitCalibration(calibration_times_);
+  time_last_ = ros::Time::now();
+  reset_swith_ = false;
+  ROS_INFO("Start MPU6050");
+}
+
+void Mpu6050::InitCalibration(int value)
+{
+  ROS_INFO("Start Calibrating MPU6050");
+
+  for (int i=0; i <value; i++)
+  {
+    GetImuData();
+    average_acceleration_[0] += acceleration_[0];
+    average_acceleration_[1] += acceleration_[1];
+    average_acceleration_[2] += acceleration_[2];
+    average_gyro_[0] += gyro_[0];
+    average_gyro_[1] += gyro_[1];
+    average_gyro_[2] += gyro_[2];
+    delay(10);
+    ROS_INFO("Calibrating MPU6050 %d / %d", i+1, value);
+  }
+
+  average_acceleration_[0] = average_acceleration_[0]/value;
+  average_acceleration_[1] = average_acceleration_[1]/value;
+  average_acceleration_[2] = average_acceleration_[2]/value;
+  average_gyro_[0] = average_gyro_[0]/value;
+  average_gyro_[1] = average_gyro_[1]/value;
+  average_gyro_[2] = average_gyro_[2]/value;
+
+  ROS_INFO("Finish Calibrating MPU6050");
+
+}
+
+void Mpu6050::Calibration()
+{
+  acceleration_[0] = acceleration_[0] - average_acceleration_[0];
+  acceleration_[1] = acceleration_[1] - average_acceleration_[1];
+  acceleration_[2] = acceleration_[2] - average_acceleration_[2];
+
+  gyro_[0] = gyro_[0] - average_gyro_[0];
+  gyro_[1] = gyro_[1] - average_gyro_[1];
+  gyro_[2] = gyro_[2] - average_gyro_[2];
+
+  gyro_[0] = DegreeToRadian(gyro_[0]);
+  gyro_[1] = DegreeToRadian(gyro_[1]);
+  gyro_[2] = DegreeToRadian(gyro_[2]);
+}
+
+void Mpu6050::GetROSParam()
+{
+  ros::NodeHandle n("~");
+  n.param<bool>("AD0", ad0_, false);
+  n.param<bool>("PublishTF", use_tf_, false);
+  n.param<bool>("UseCalibration", use_calibration_, true);
+
+  n.param<int>("CalibrationTimes", calibration_times_, 2000);
+  if(calibration_times_ <= 0) calibration_times_ = 0;
+
+  n.param<bool>("UseFileter", use_fileter_, true);
+
+  n.param<double>("FileterGain", beta_, 0.0001);
+  if(beta_ <= 0.0) beta_ = 0.0;
+
+  n.param<int>("AccelerationScale", acceleration_scale_, 3);
+  if(acceleration_scale_ < 0 | acceleration_scale_ > 3) acceleration_scale_ = 3;
+
+  n.param<int>("GyroScale", gyro_scale_, 3);
+  if(gyro_scale_ < 0 | gyro_scale_ > 3) gyro_scale_ = 3;
+
+  n.param<std::string>("frame_id", frame_id_, "imu_link");
+  n.param<std::string>("parents_frame_id", parents_frame_id_, "base_link");
+
+  ROS_INFO("AccelerationScale : %d", acceleration_scale_);
+  ROS_INFO("GyroScale : %d", gyro_scale_);
+}
+
+void Mpu6050::SetUp()
+{
+  if(ad0_ == true) fd_ = wiringPiI2CSetup(ADDRESS_AD0_HIGH);
+  else fd_ = wiringPiI2CSetup(ADDRESS_AD0_LOW);
+}
+
+void Mpu6050::InitSensor()
+{
+  wiringPiI2CWriteReg8 (fd_, SMPLRT_DIV, 0x07);
+  wiringPiI2CWriteReg8 (fd_, PWR_MGMT_1, 0x04);
+  wiringPiI2CWriteReg8 (fd_, CONFIG, 0x00);
+
+  if(acceleration_scale_ == 0) wiringPiI2CWriteReg8 (fd_, ACCEL_CONFIG, 0x00);
+  else if(acceleration_scale_ == 1) wiringPiI2CWriteReg8 (fd_, ACCEL_CONFIG, 0x08);
+  else if(acceleration_scale_ == 2) wiringPiI2CWriteReg8 (fd_, ACCEL_CONFIG, 0x10);
+  else if(acceleration_scale_ == 3) wiringPiI2CWriteReg8 (fd_, ACCEL_CONFIG, 0x18);
+  else wiringPiI2CWriteReg8 (fd_, ACCEL_CONFIG, 0x00);
+
+  if(gyro_scale_ == 0) wiringPiI2CWriteReg8 (fd_, GYRO_CONFIG, 0x00);
+  else if(gyro_scale_ == 1) wiringPiI2CWriteReg8 (fd_, GYRO_CONFIG, 0x08);
+  else if(gyro_scale_ == 2) wiringPiI2CWriteReg8 (fd_, GYRO_CONFIG, 0x10);
+  else if(gyro_scale_ == 3) wiringPiI2CWriteReg8 (fd_, GYRO_CONFIG, 0x18);
+  else wiringPiI2CWriteReg8 (fd_, GYRO_CONFIG, 0x00);
+
+  wiringPiI2CWriteReg8 (fd_, INT_ENABLE, 0x01);
+  wiringPiI2CWriteReg8 (fd_, SMPLRT_DIV, 0x07);
+}
+
+short Mpu6050::ReadRawData(int addr)
+{
+  short high_byte,low_byte,value;
+  high_byte = wiringPiI2CReadReg8(fd_, addr);
+  low_byte = wiringPiI2CReadReg8(fd_, addr+1);
+  value = (high_byte << 8) | low_byte;
+  return value;
+}
+
+void Mpu6050::GetImuData()
+{
+  acceleration_[0] = (ReadRawData(ACCEL_XOUT_H) / (16384.0 / pow(2, acceleration_scale_))) * 9.80665;
+  acceleration_[1] = (ReadRawData(ACCEL_YOUT_H) / (16384.0 / pow(2, acceleration_scale_))) * 9.80665;
+  acceleration_[2] = (ReadRawData(ACCEL_ZOUT_H) / (16384.0 / pow(2, acceleration_scale_))) * 9.80665;
+  gyro_[0] = ReadRawData(GYRO_XOUT_H) / (131.072 / pow(2, gyro_scale_));
+  gyro_[1] = ReadRawData(GYRO_YOUT_H) / (131.072 / pow(2, gyro_scale_));
+  gyro_[2] = ReadRawData(GYRO_ZOUT_H) / (131.072 / pow(2, gyro_scale_));
+}
+
+void Mpu6050::CalculationQuaternion(double gx, double gy, double gz, double ax, double ay, double az)
+{
+  double recipNorm;
+  double s0, s1, s2, s3;
+  double qDot1, qDot2, qDot3, qDot4;
+  double _2q0, _2q1, _2q2, _2q3, _4q0, _4q1, _4q2 ,_8q1, _8q2, q0q0, q1q1, q2q2, q3q3;
+
+  // Rate of change of quaternion from gyroscope
+  qDot1 = 0.5 * (-q1_ * gx - q2_ * gy - q3_ * gz);
+  qDot2 = 0.5 * (q0_ * gx + q2_ * gz - q3_ * gy);
+  qDot3 = 0.5 * (q0_ * gy - q1_ * gz + q3_ * gx);
+  qDot4 = 0.5 * (q0_ * gz + q1_ * gy - q2_ * gx);
+
+  if(use_fileter_ == true)
+  {
+    // Compute feedback only if accelerometer measurement valid (avoids NaN in accelerometer normalisation)
+    if(!((ax == 0.0) && (ay == 0.0) && (az == 0.0))) {
+
+      // Normalise accelerometer measurement
+      recipNorm = sqrt(ax * ax + ay * ay + az * az);
+      recipNorm = 1.0/recipNorm;
+      ax *= recipNorm;
+      ay *= recipNorm;
+      az *= recipNorm;
+
+      // Auxiliary variables to avoid repeated arithmetic
+      _2q0 = 2.0 * q0_;
+      _2q1 = 2.0 * q1_;
+      _2q2 = 2.0 * q2_;
+      _2q3 = 2.0 * q3_;
+      _4q0 = 4.0 * q0_;
+      _4q1 = 4.0 * q1_;
+      _4q2 = 4.0 * q2_;
+      _8q1 = 8.0 * q1_;
+      _8q2 = 8.0 * q2_;
+      q0q0 = q0_ * q0_;
+      q1q1 = q1_ * q1_;
+      q2q2 = q2_ * q2_;
+      q3q3 = q3_ * q3_;
+
+      // Gradient decent algorithm corrective step
+      s0 = _4q0 * q2q2 + _2q2 * ax + _4q0 * q1q1 - _2q1 * ay;
+      s1 = _4q1 * q3q3 - _2q3 * ax + 4.0 * q0q0 * q1_ - _2q0 * ay - _4q1 + _8q1 * q1q1 + _8q1 * q2q2 + _4q1 * az;
+      s2 = 4.0 * q0q0 * q2_ + _2q0 * ax + _4q2 * q3q3 - _2q3 * ay - _4q2 + _8q2 * q1q1 + _8q2 * q2q2 + _4q2 * az;
+      s3 = 4.0 * q1q1 * q3_ - _2q1 * ax + 4.0 * q2q2 * q3_ - _2q2 * ay;
+      recipNorm = sqrt(s0 * s0 + s1 * s1 + s2 * s2 + s3 * s3); // normalise step magnitude
+      recipNorm = 1.0/recipNorm;
+      s0 *= recipNorm;
+      s1 *= recipNorm;
+      s2 *= recipNorm;
+      s3 *= recipNorm;
+
+      // Apply feedback step
+      qDot1 -= beta_ * s0;
+      qDot2 -= beta_ * s1;
+      qDot3 -= beta_ * s2;
+      qDot4 -= beta_ * s3;
+    }
+  }
+
+  // Integrate rate of change of quaternion to yield quaternion
+  q0_ += qDot1 * (1.0 * dt_);
+  q1_ += qDot2 * (1.0 * dt_);
+  q2_ += qDot3 * (1.0 * dt_);
+  q3_ += qDot4 * (1.0 * dt_);
+
+  // Normalise quaternion
+  recipNorm = sqrt(q0_ * q0_ + q1_ * q1_ + q2_ * q2_ + q3_ * q3_);
+  recipNorm = 1.0/recipNorm;
+  q0_ *= recipNorm;
+  q1_ *= recipNorm;
+  q2_ *= recipNorm;
+  q3_ *= recipNorm;
+
+}
+
+void Mpu6050::UpdateImuData()
+{
+  imu_.header.stamp = time_now_;
+  imu_.header.frame_id = frame_id_;
+  imu_.orientation.x = q1_;
+  imu_.orientation.y = q2_;
+  imu_.orientation.z = q3_;
+  imu_.orientation.w = q0_;
+  imu_.linear_acceleration.x = acceleration_[0];
+  imu_.linear_acceleration.y = acceleration_[1];
+  imu_.linear_acceleration.z = acceleration_[2];
+  imu_.angular_velocity.x = gyro_[0];
+  imu_.angular_velocity.y = gyro_[1];
+  imu_.angular_velocity.z = gyro_[2];
+}
+
+void Mpu6050::UpdateTF()
+{
+  orientation_[0] = q1_;
+  orientation_[1] = q2_;
+  orientation_[2] = q3_;
+  orientation_[3] = q0_;
+
+  if(use_tf_ == true)
+  {
+    broadcaster_.sendTransform(tf::StampedTransform(tf::Transform(orientation_, tf::Vector3(0.0, 0.0, 0.0)),
+                                                    time_now_,
+                                                    parents_frame_id_,
+                                                    frame_id_));
+  }
+
+}
+
+void Mpu6050::UpdateRPY()
+{
+  tf::Matrix3x3 m(orientation_);
+  double roll,pitch,yaw;
+  m.getRPY(roll,pitch,yaw);
+
+  rpy_.TimeStamp = time_now_;
+  rpy_.roll = RadianToDegree(roll);
+  rpy_.pitch = RadianToDegree(pitch);
+  rpy_.yaw = RadianToDegree(yaw);
+}
+
+void Mpu6050::Update()
+{
+  UpdateImuData();
+  UpdateTF();
+  UpdateRPY();
+}
+
+void Mpu6050::Publisher()
+{
+  publisher_imu_.publish(imu_);
+  publisher_rpy_.publish(rpy_);
+}
+
+void Mpu6050::Spin()
+{
+  if(reset_swith_ == true) Reset();
+  time_now_ = ros::Time::now();
+  CalculationDeltaTime();
+  GetImuData();
+  Calibration();
+  CalculationQuaternion(gyro_[0],gyro_[1],gyro_[2],acceleration_[0],acceleration_[1],acceleration_[2]);
+  Update();
+  Publisher();
+  time_last_ = time_now_;
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "mpu6050_node");
+    ros::NodeHandle n;
+    ros::NodeHandle pn;
+
+    int hz = 60;
+
+    pn.param<int>("Hz", hz, 60);
+    if(hz <= 0) hz = 60;
+
+    ROS_INFO("mpu6050_node Open");
+
+    ros::Rate loop_rate(hz);
+
+    Mpu6050 mpu6050(n);
+
+    while (ros::ok())
+    {
+      mpu6050.Spin();
+      ros::spinOnce();
+      loop_rate.sleep();
+    }
+
+    ROS_INFO("mpu6050_node Close");
+
+    return 0;
+}
